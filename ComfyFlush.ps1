d5ac15dff2513b2ba949c3c1d7cb3b0d5e65064a
Hugo <63236244+hugoremington@users.noreply.github.com>
2026-04-18 01:23:24 +1000

    Updated README.md
    Minor formatting fixes and metadata update.

--- ComfyFlush.ps1
+++ ComfyFlush.ps1
@@ -12,7 +12,7 @@ try
     while($True)
     {
         # --- CONFIGURATION ---
-        $ComfyDir      = "C:\Program Files\ComfyUI"
+        $ComfyDir      = "C:\Program Files\StabilityMatrix\Packages\ComfyUI"
         $VenvScripts   = "$ComfyDir\venv\Scripts"
         $LogFile       = "$ComfyDir\comfyui.log"
         $Timeout       = 595 # 5 seconds under 10 minutes. Must reflect with your scheduled task frequency.
@@ -22,8 +22,7 @@ try
             # Write-Host "ComfyUI directory path does not exist. Please retry."
         }
 
-        # Inject the VENV into the session PATH
-        # This allows us to call "python" directly as a native command
+        # Inject the VENV into the session PATH this allows us to call "python" directly as a native command.
         $env:PATH = "$VenvScripts;" + $env:PATH
 
         # Change directory to ComfyUI (Required so main.py finds its files)
@@ -34,9 +33,7 @@ try
         # Write-Host "Launching ComfyUI via native venv 'python'..."
         # Write-Host "Logging to: $LogFile"
 
-        # Start the process
-        # Because 'python' is now in the PATH, we no longer need the long, problematic path variable.
-        # We use 'cmd /c' only to handle the log redirection (>>).
+        # Using 'cmd /c' only to handle the log redirection (>>).
         $Command = "cmd /c python main.py >> `"$LogFile`" 2>&1"
 
         $process = Start-Process -FilePath "cmd.exe" -ArgumentList "/c $Command" -PassThru -WindowStyle Hidden
@@ -48,8 +45,9 @@ try
         # Termination Logic
         if (-not $process.HasExited) {
             # Write-Host "Timeout reached! Forcefully terminating process tree..." -ForegroundColor Red
-            # Kill the CMD process and everything it started (the Python process)
+            # Kill the CMD processand everything it started (Python process).
             taskkill /F /T /PID $process.Id | Out-Null
+            # Kill the parent PowerShell console.
             taskkill /F /T /PID $parent.Id | Out-Null
             " [TIMEOUT] $(Get-Date) - Process terminated by watchdog." | Out-File -FilePath $LogFile -Append
         } else {
@@ -63,6 +61,7 @@ Finally
     # Write-Host "Exit received. Forcefully terminating process tree..." -ForegroundColor Yellow
     # Kill the CMD process and everything it started (the Python process)
     taskkill /F /T /PID $process.Id | Out-Null
+    # Kill the parent PowerShell console.
     taskkill /F /T /PID $parent.Id | Out-Null
     " [TIMEOUT] $(Get-Date) - Process terminated by watchdog." | Out-File -FilePath $LogFile -Append
 }
